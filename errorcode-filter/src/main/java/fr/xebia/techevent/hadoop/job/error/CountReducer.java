a57891770c2090849b2c5823726b117acd50ad45
Bertrand Dechoux <dechouxb@gmail.com>
2012-09-17 23:49:46 +0200

    for each

--- errorcode-filter/src/main/java/fr/xebia/techevent/hadoop/job/error/CountReducer.java
+++ errorcode-filter/src/main/java/fr/xebia/techevent/hadoop/job/error/CountReducer.java
@@ -1,21 +1,19 @@
 package fr.xebia.techevent.hadoop.job.error;
 
-import fr.xebia.techevent.hadoop.job.error.model.ErrorInfo;
+import java.io.IOException;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
-import java.io.IOException;
-import java.util.Iterator;
+import fr.xebia.techevent.hadoop.job.error.model.ErrorInfo;
 
 public class CountReducer extends Reducer<ErrorInfo, IntWritable, ErrorInfo, IntWritable> {
     @Override
     protected void reduce(ErrorInfo key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         int count = 0;
-        Iterator<IntWritable> it = values.iterator();
-        while (it.hasNext()) {
-            count += it.next().get();
+        for (IntWritable value : values) {
+            count += value.get();
         }
-
         context.write(key, new IntWritable(count));
     }
 }
